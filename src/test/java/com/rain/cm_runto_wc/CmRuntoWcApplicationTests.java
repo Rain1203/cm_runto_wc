297c3dc898643589e2086f1e0f9d0283461c1b93
qinxiaoyu <qinxiaoyu@xing-ray.com>
2021-05-14 16:29:55 +0800

    cm找厕所项目代码初始化

---
+++ src/test/java/com/rain/cm_runto_wc/CmRuntoWcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rain.cm_runto_wc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmRuntoWcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
